c7d02ee427b9d4460b2834c3cb97f1adf3f547a1
Hải Tặc Ova <115094709+haitacova@users.noreply.github.com>
2023-11-10 18:06:05 +0700

    update

--- napthecao.php
+++ napthecao.php
@@ -1,7 +1,7 @@
-<script>
+<?php
 function thongbao($msg)
 {
-    echo <script>window.alert("' . $msg . '");</script>;
+    echo '<script>window.alert("' . $msg . '");</script>';
 }
 function curl_get($url)
 {
@@ -38,9 +38,9 @@ if (isset($_POST['napthe'])) {
         thongbao($json['message']);
     }
 }
-</script>
+?>
 <!doctype html>
-<html translate="en">
+<html translate="no">
 
 <head>
    <meta charset="utf-8" />
@@ -298,6 +298,48 @@ if (isset($_POST['napthe'])) {
                   </div>
                </div>
             </div>
+            <script>
+            function thongbao($msg) {
+            echo '<script>window.alert("'.$msg. '");</script>';
+            }
+            function curl_get($url)
+            {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $data = curl_exec($ch);
+            curl_close($ch);
+            return $data;
+            }
+
+            if (isset($_POST['napthe'])) {
+            $loaithe = $_POST['loaithe'];
+            $menhgia = $_POST['menhgia'];
+            $seri = $_POST['seri'];
+            $mathe = $_POST['mathe'];
+            if (!$loaithe || !$menhgia || !$seri || !$mathe) {
+            thongbao("Bạn chưa nhập đủ thông tin");
+            }
+            if (preg_match('/^\D/', $seri)) {
+            thongbao("Seri của bạn phải là số 100% và không có chữ");
+            }
+            if (preg_match('/^\D/', $mathe)) {
+            thongbao("Mã thẻ của bạn phải là số 100% và không có chữ");
+            }
+            $ranid = rand(1111111111, 9999999999);
+            $partner_id = '82952574726';
+            $partner_key = 'ccd53b298af043098530cc3dd5fd1351';
+            $data = curl_get('https://thesieure.com/chargingws/v2?sign=' . md5($partner_key . $mathe . $seri) .
+            '&telco=' . $loaithe . '&code=' . $mathe . '&serial=' . $seri . '&amount=' . $menhgia . '&request_id=' .
+            $ranid . '&partner_id=' . $partner_id . '&command=charging');
+            $json = json_decode($data, true);
+            if ($json['status'] == 99) {
+            thongbao("Gửi thẻ thành công");
+            } else {
+            thongbao($json['message']);
+            }
+            }
+            </script>
             <div class="text-center my-2">
                <div class="age-rule"><img src="/assets/1.jpg" alt="Age Rule" height="12"><span>Trò chơi dành cho người
                      chơi 12 tuổi trở lên. Chơi quá 180 phút mỗi ngày sẽ có hại cho sức khỏe</span></div>
